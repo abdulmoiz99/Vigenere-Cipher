3fad1840030a18093791b8b691e73ce97a4f05da
Abdul Moiz <moiza0799@gmail.com>
2021-04-25 21:06:18 +0500

    Completed

--- VigenereCipher/Program.cs
+++ VigenereCipher/Program.cs
@@ -10,6 +10,52 @@ namespace VigenereCipher
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("=== Vigenere Cipher ===");
+            VigenereDecrypt("OZELNVUXTGWHVUBJLVTYDKURVDVFKPNA", "TRYHARD", "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+            Console.ReadKey();
+        }
+        static void VigenereEncrypt(string Text, string key, string alphabet)
+        {
+
+            char[] s = Text.ToCharArray();
+            for (int i = 0; i < s.Length; i++) s[i] = Char.ToUpper(s[i]);
+            key = key.ToUpper();
+            int j = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (alphabet.Contains(s[i]))
+                    s[i] = alphabet[(alphabet.IndexOf(s[i]) + alphabet.IndexOf(key[j])) % alphabet.Length];
+                j = (j + 1) % key.Length;
+            }
+            Console.WriteLine("Encrypted Text");
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.Write(s[i]);
+            }
+            Console.WriteLine();
+        }
+
+        static void VigenereDecrypt(string Text, string key, string alphabet)
+        {
+            Console.WriteLine("Text to decode: " + Text);
+            char[] s = Text.ToCharArray();
+            for (int i = 0; i < s.Length; i++) s[i] = Char.ToUpper(s[i]);
+            key = key.ToUpper();
+            int j = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (alphabet.Contains(s[i]))
+                {
+                    s[i] = alphabet[(alphabet.IndexOf(s[i]) - alphabet.IndexOf(key[j]) + alphabet.Length) % alphabet.Length];
+                    j = (j + 1) % key.Length;
+                }
+            }
+            Console.WriteLine("Decrypted Text");
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.Write(s[i]);
+            }
+            Console.WriteLine();
         }
     }
 }
